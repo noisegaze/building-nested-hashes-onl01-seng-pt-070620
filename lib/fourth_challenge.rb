b6f5909a5eaf789d57063d144fcdd4391af530f2
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-17 19:45:07 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -10,7 +10,9 @@ def fourth_challenge
           :age => "17",
           :attitude => "worried"
         },{
-          
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
         }
       ]
    }, 
